5a7d0533427b4101208dd6c419a4d68178d0b054
MysteriousLog6 <63236194+MysteriousLog6@users.noreply.github.com>
2022-12-10 23:41:04 +0000

    Add preliminary support for fw_info.sh

--- fw_info.sh
+++ fw_info.sh
@@ -3,10 +3,12 @@
 echo "Enter filename or path: "
 read FWFILE
 
-KONGMING=$(grep 'Smart Band 5' $FWFILE)
 PANGU=$(grep 'Smart Band 6' $FWFILE)
+KONGMING=$(grep 'Smart Band 5' $FWFILE)
 CINCO=$(grep 'Smart Band 4' $FWFILE)
 BAND=$(grep 'Xiaomi Band 3' $FWFILE)
+BANDTWO=$(grep 'MI Band 2' $FWFILE)
+BANDHRX=$(grep 'Mi Band HRX' $FWFILE)
 
 fwheader()
 {
@@ -17,6 +19,18 @@ fwheader()
 fwheader_band3()
 {
 	FW_VER=$(tail -c +520 $FWFILE | head -c 9)
+	echo Firmware Version: $FW_VER
+}
+
+fwheader_band2()
+{
+	FW_VER=$(tail -c +153074 $FWFILE | head -c 9)
+	echo Firmware Version: $FW_VER
+}
+
+fwheader_bandHRX()
+{
+	FW_VER=$(tail -c +145056 $FWFILE | head -c 9)
 	echo Firmware Version: $FW_VER
 }
 
@@ -36,4 +50,12 @@ elif [ "$BAND" == "Binary file $FWFILE matches" ]
 then
 	echo Device: Mi Band 3
 	fwheader_band3
+elif [ "$BANDTWO" == "Binary file $FWFILE matches" ]
+then
+	echo Device: Mi Band 2
+	fwheader_band2
+elif [ "$BANDHRX" == "Binary file $FWFILE matches" ]
+then
+	echo Device: Mi Band HRX
+	fwheader_bandHRX
 fi
